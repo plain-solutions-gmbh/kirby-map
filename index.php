684d8d32a963f6cc4d66bc0f69c38ff510937c19
Johann Schopplich <mail@johannschopplich.com>
2021-09-29 21:13:40 +0200

    Consistent formatting in index.php

--- index.php
+++ index.php
@@ -1,32 +1,29 @@
 <?php
 
-use Kirby\Text\Markdown;
-
 Kirby::plugin('microman/map', [
-    'options' => array(
-		'token'         => 'pk.eyJ1IjoibWljcm9tYW4iLCJhIjoiY2txOWg0ZDg2MDJqdDJxbW9sMGNhbjFwaCJ9.j7h8Wv0LnS2QqmuL7VR6wQ',
+    'options' => [
+        'token'         => 'pk.eyJ1IjoibWljcm9tYW4iLCJhIjoiY2txOWg0ZDg2MDJqdDJxbW9sMGNhbjFwaCJ9.j7h8Wv0LnS2QqmuL7VR6wQ',
         'defaultStyle'  => 'streets-v11',
-	),
+    ],
     'blueprints' => [
         'blocks/maps' => __DIR__ . '/blueprints/blocks/maps.yml',
         'blocks/marker' => __DIR__ . '/blueprints/blocks/marker.yml',
     ],
-    'icons' => [],
     'fields' => [
         'geolocation' => [
             'props' => [
-                'token' => function() {
+                'token' => function () {
                     return option('microman.map.token');
                 }
             ]
         ]
     ],
-    'fieldMethods' => array(
+    'fieldMethods' => [
         'toLocation' => function ($field) {
             $structure = new Structure([$field->yaml()], $field->parent());
             return $structure->first();
         }
-    ),
+    ],
     'snippets' => [
         'blocks/maps' => __DIR__ . '/snippets/blocks/maps.php',
     ],
@@ -42,14 +39,15 @@ Kirby::plugin('microman/map', [
                 'pattern' => 'map/converter',
                 'method' => 'POST',
                 'action' => function () {
-                    return array('html' => kt(get('markdown')));
+                    return [
+                        'html' => kt(get('markdown'))
+                    ];
                 }
             ]
         ]
-            ],
-    'translations' => array(
-        'en' => require_once __DIR__ . '/lib/languages/en.php',
-        'de' => require_once __DIR__ . '/lib/languages/de.php'
-    ),
-
+    ],
+    'translations' => [
+        'en' => require __DIR__ . '/lib/languages/en.php',
+        'de' => require __DIR__ . '/lib/languages/de.php'
+    ]
 ]);

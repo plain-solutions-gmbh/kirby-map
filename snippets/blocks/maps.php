d77cfe05697c02075cf9f59a999dc2696c2f9cf6
Johann Schopplich <mail@johannschopplich.com>
2021-09-29 21:15:57 +0200

    Refactor maps block

--- snippets/blocks/maps.php
+++ snippets/blocks/maps.php
@@ -1,4 +1,4 @@
-<div id='map_<?= $block->id() ?>' class="block" style='width: 100%; height: 100%;min-height: 400px;'></div>
+<div id="embedded-map-<?= $block->id() ?>" class="block" style="width: 100%; height: 100%; min-height: 400px;"></div>
 
 <script src="https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.js"></script>
 <link href="https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.css" rel="stylesheet">
@@ -6,46 +6,39 @@
 <?php $center = $block->center()->toLocation(); ?>
 
 <script>
-    mapboxgl.accessToken = '<?= $kirby->option('microman.map.token') ?>';
-    var map = new mapboxgl.Map({
-        container: 'map_<?= $block->id() ?>',
-        style: 'mapbox://styles/mapbox/<?= $block->style() ?>',
-        center: [<?= str_replace(',', '.', $center->lng()) ?>, <?= str_replace(',', '.', $center->lat()) ?>],
-        zoom: <?= ($block->zoom()->isNotEmpty()) ? $block->zoom() : 12 ?>
-    });
+mapboxgl.accessToken = "<?= option('katapult.shared.maps.token') ?>";
+const map = new mapboxgl.Map({
+  container: "embedded-map-<?= $block->id() ?>",
+  style: "mapbox://styles/mapbox/<?= $block->style() ?>",
+  center: [<?= str_replace(',', '.', $center->lng()) ?>, <?= str_replace(',', '.', $center->lat() ) ?>],
+  zoom: <?= $block->zoom()->or(12) ?>
+});
 
-    <?php foreach ($block->marker()->toBlocks() as $marker) : ?>
+<?php foreach ($block->marker()->toBlocks() as $marker): ?>
+  <?php $coordinates = $marker->coors()->toLocation() ?>
+  <?php $markerid = 'elMarker' . substr($marker->id(), 0, 8) ?>
+  let <?= $markerid ?> = null;
 
+  <?php if ($image = $marker->image()->toFile()): ?>
+    <?= $markerid ?> = document.createElement('div');
+    <?= $markerid ?>.className = 'marker';
+    <?= $markerid ?>.style.backgroundImage = "url(<?= $image->url() ?>)";
+    <?= $markerid ?>.style.width = "<?= ($image->width() / 100) * $marker->size()->int() ?>px";
+    <?= $markerid ?>.style.height = "<?= ($image->height() / 100) * $marker->size()->int() ?>px";
+    <?= $markerid ?>.style.backgroundSize = '100%';
+  <?php endif ?>
 
-        <?php $coors = $marker->coors()->toLocation() ?>
-        <?php $markerid = 'elMarker' . substr($marker->id(), 0, 8) ?>
-
-        let <?= $markerid ?> = null
-
-        <?php if ($image = $marker->image()->toFile()) : ?>
-
-            <?= $markerid ?> = document.createElement('div');
-            <?= $markerid ?>.className = 'marker';
-            <?= $markerid ?>.style.backgroundImage = "url('<?= $image->url() ?>')";
-            <?= $markerid ?>.style.width = "<?= $image->width() / 100 * $marker->size()->int() ?>px";
-            <?= $markerid ?>.style.height = "<?= $image->height() / 100 * $marker->size()->int() ?>px";
-            <?= $markerid ?>.style.backgroundSize = '100%';
-
-        <?php endif ?>
-
-        new mapboxgl.Marker({
-                anchor: '<?= $marker->anchor() ?>',
-                element: <?= $markerid ?>
-            })
-            .setLngLat([<?= str_replace(',', '.', $coors->lng()) ?>, <?= str_replace(',', '.', $coors->lat()) ?>])
-            .addTo(map)
-
-        <?php if ($marker->haspopup()->isTrue()) : ?>
-                .setPopup(new mapboxgl.Popup({
-                        offset: <?= $marker->popupoffset() ?>
-                    })
-                    .setHTML(`<?= $marker->popup()->kt() ?>`));
-        <?php endif ?>
-
-    <?php endforeach ?>
+  new mapboxgl.Marker({
+    anchor: "<?= $marker->anchor() ?>",
+    element: <?= $markerid ?>
+  })
+    .setLngLat([<?= str_replace(',', '.', $coordinates->lng()) ?>, <?= str_replace(',', '.', $coordinates->lat()) ?>])
+    .addTo(map)
+  <?php if ($marker->haspopup()->isTrue()): ?>
+    .setPopup(new mapboxgl.Popup({
+      offset: <?= $marker->popupoffset() ?>
+    })
+    .setHTML('<?= $marker->popup()->kt() ?>'));
+  <?php endif ?>
+<?php endforeach ?>
 </script>

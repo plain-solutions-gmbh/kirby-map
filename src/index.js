2af96281f8835ab259e20818b97eab878d8c4fe1
Roman Gsponer <mail@microman.ch>
2025-03-20 16:53:26 +0100

    fix utils

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import Maps from "@/components/Map.vue";
 import Marker from "@/components/Marker.vue";
 import Geolocation from "@/components/Geolocation.vue";
 import SearchDialog from "@/components/SearchDialog.vue";
-import PlainLicense from "./components/PlainLicense.vue";
+import PlainLicense from "../utils/PlainLicense.vue";
 
 window.panel.plugin("plain/map", {
   blocks: {
